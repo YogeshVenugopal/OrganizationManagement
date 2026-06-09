d747bf7dbd917749a5f3886b945c1d284642cfd5
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-06-09 21:46:13 +0530

    Updated the backend

---
+++ Server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+
+app.listen(PORT, () => {
+    console.log(`The server is started running on port : ${PORT}`);
+})
